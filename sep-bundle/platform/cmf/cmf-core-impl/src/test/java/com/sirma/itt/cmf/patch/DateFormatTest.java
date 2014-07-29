94dacbf546ea3a778e1024dc4b9dae816c726eb9
SirmaITT <itt-open-source@sirma.bg>
2014-07-29 10:09:55 +0300

    Make tests timezone-independent

--- sep-bundle/platform/cmf/cmf-core-impl/src/test/java/com/sirma/itt/cmf/patch/DateFormatTest.java
+++ sep-bundle/platform/cmf/cmf-core-impl/src/test/java/com/sirma/itt/cmf/patch/DateFormatTest.java
@@ -1,5 +1,7 @@
 package com.sirma.itt.cmf.patch;
 
+import java.util.TimeZone;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,7 +17,21 @@ public class DateFormatTest {
 	 */
 	@Test
 	public void testParser() {
-		String isoDate = "2013-05-27T00:00:00.000+03:00";
+		int offset = TimeZone.getDefault().getOffset(System.currentTimeMillis()) / (60 * 60 * 1000);
+		StringBuilder offsetBuilder = new StringBuilder();
+		offsetBuilder.append(offset);
+
+		if (offset >= 0) {
+			offsetBuilder.insert(0, "+");
+		}
+
+		if (Math.abs(offset) < 10) {
+			offsetBuilder.insert(1, "0");
+		}
+
+		offsetBuilder.append(":00");
+
+		String isoDate = "2013-05-27T00:00:00.000" + offsetBuilder.toString();
 		Assert.assertEquals(isoDate, ISO8601DateFormat.format(ISO8601DateFormat.parse(isoDate)));
 	}
 
